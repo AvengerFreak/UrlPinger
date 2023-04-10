573311a7f17952546ff787619383402d0f206397
AvengerFreak <starwarsgeek14mz@gmail.com>
2023-04-09 20:31:01 -0500

    initial commit

---
+++ url_pinger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace url_pinger{
+
+    class Program{
+
+        static void Main(string[] args){
+            
+            var ping = new System.Net.NetworkInformation.Ping();
+            var result = ping.Send("www.google.com");
+
+            if (result.Status != System.Net.NetworkInformation.IPStatus.Success){
+                Console.WriteLine("Service is down");
+            }
+                
+            else
+            {
+                Console.WriteLine("Service is up");
+            }
+
+        }
+    }
+}
